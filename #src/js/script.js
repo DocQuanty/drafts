268ce633f3c5257e418bd574c59624485811d861
Urchikak8@gmail.com <Urchikak8@gmail.com>
2021-12-25 13:55:18 +0200

    create shedulsResults

--- #src/js/script.js
+++ #src/js/script.js
@@ -25,3 +25,31 @@ $(document).ready(function () {
     ],
   });
 });
+
+$(document).ready(function () {
+  $(".sliderCards").slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    speed: 1500,
+    swipe: false,
+    swipeToSlide: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+        },
+      },
+
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ],
+  });
+});
